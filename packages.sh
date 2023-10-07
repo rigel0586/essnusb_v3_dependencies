7659ad0184c2307ff2e818a7024f02936fa6a1e4
rigel0586 <rigel0586@gmail.com>
2023-10-07 23:19:22 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get -y install repo cmake dpkg-dev g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev libssl-dev cmake fastlink gfortran libtool automake autoconf autotools-dev python2 python3 build-essential libxmu-dev libxi-dev libgl-dev binutils libglu1-mesa freeglut3-dev
 sudo apt-get -y install dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev libssl-dev gfortran libpcre3-dev xlibmesa-glu-dev libglew-dev libftgl-dev libmysqlclient-dev libfftw3-dev libcfitsio-dev graphviz-dev libavahi-compat-libdnssd-dev libldap2-dev libxml2-dev libkrb5-dev libgsl0-dev qtwebengine5-dev
-sudo apt-get -y install protobuf-compiler python2-dev python3-numpy subversion
+sudo apt-get -y install protobuf-compiler python2-dev python3-numpy subversion doxygen
 
