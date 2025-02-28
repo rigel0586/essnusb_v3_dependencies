19c44e3cd33c4b67066cbc07b06cd82d74f8f6ef
rigel0586 <rigel0586@gmail.com>
2025-02-28 23:28:25 +0200

    Add pyenv python for lhapdf build

--- genie/build.sh
+++ genie/build.sh
@@ -20,47 +20,60 @@ base_directory=${PWD}
 
 genie_dir=genie
 
-#genie_lhapdf5=lhapdf-5.9.1.tar.gz
-#genie_lhapdf6=LHAPDF-6.5.4.tar.gz
+genie_lhapdf5=lhapdf-5.9.1.tar.gz
+genie_lhapdf6=LHAPDF-6.5.4.tar.gz
 genie_log4cpp=log4cpp-1.1.4.tar.gz
 
-#genie_lhapdf5_dir=${dependency_dir}/${genie_dir}/lhapdf-5.9.1
-#genie_lhapdf6_dir=${dependency_dir}/${genie_dir}/LHAPDF-6.5.4
+genie_lhapdf5_dir=${dependency_dir}/${genie_dir}/lhapdf-5.9.1
+genie_lhapdf6_dir=${dependency_dir}/${genie_dir}/LHAPDF-6.5.4
 genie_log4cpp_dir=${dependency_dir}/${genie_dir}/log4cpp
 genie_pythia6_dir=${dependency_dir}/${genie_dir}/pythia6
 
 # backup python link
 #sudo cp /usr/bin/python /usr/bin/python_up
 
+cd ${dependency_dir}
+cd ..
+
+# Set python env
+third_party_dir=$base_directory
+export PYENV_ROOT=$third_party_dir/pyenv
+export PATH=$PATH:$third_party_dir/pyenv/bin
+
+pyenv install -f 2.7.18
+pyenv install -f 3.12.9
+
+export BACK_UP_PATH=$PATH
+export PATH=$BACK_UP_PATH:$PYENV_ROOT/versions/2.7.18/bin
+
 # lhapdf5
-#cd ${dependency_dir}/${genie_dir}
-#tar -xf ${genie_lhapdf5}
-#cd ${genie_lhapdf5_dir}
-#mkdir build
-#mkdir install
-#sudo cp /usr/bin/python2 /usr/bin/python
-#export FCFLAGS=-std=legacy
-#./configure --prefix=${genie_lhapdf5_dir}/install
-#gmake 
-#gmake install
-#lhapdf5lib=${genie_lhapdf5_dir}/install/lib/libLHAPDF.so
-#check_lib_exists ${lhapdf5lib}
+cd ${dependency_dir}/${genie_dir}
+tar -xf ${genie_lhapdf5}
+cd ${genie_lhapdf5_dir}
+mkdir build
+mkdir install
+export PATH=$BACK_UP_PATH:$PYENV_ROOT/versions/2.7.18/bin
+export FCFLAGS=-std=legacy
+./configure --prefix=${genie_lhapdf5_dir}/install
+gmake 
+gmake install
+lhapdf5lib=${genie_lhapdf5_dir}/install/lib/libLHAPDF.so
+check_lib_exists ${lhapdf5lib}
 
 # lhapdf6
-#cd ${dependency_dir}/${genie_dir}
-#tar -xf ${genie_lhapdf6}
-#cd ${genie_lhapdf6_dir}
-#mkdir build
-#mkdir install
-#sudo cp /usr/bin/python3 /usr/bin/python
-#./configure --prefix=${genie_lhapdf6_dir}/install
-#gmake 
-#gmake install
-#lhapdf6lib=${genie_lhapdf6_dir}/install/lib/libLHAPDF.so
-#check_lib_exists ${lhapdf6lib}
+cd ${dependency_dir}/${genie_dir}
+tar -xf ${genie_lhapdf6}
+cd ${genie_lhapdf6_dir}
+mkdir build
+mkdir install
+export PATH=$BACK_UP_PATH:$PYENV_ROOT/versions/3.12.9/bin
+./configure --prefix=${genie_lhapdf6_dir}/install
+gmake 
+gmake install
+lhapdf6lib=${genie_lhapdf6_dir}/install/lib/libLHAPDF.so
+check_lib_exists ${lhapdf6lib}
 
-# restore python link
-#sudo cp /usr/bin/python_up /usr/bin/python
+export PATH=$BACK_UP_PATH
 
 # log4cpp
 cd ${dependency_dir}/${genie_dir}
